7fd561b259d4051fac03e535c5150e92faee6ada
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-11-09 19:17:25 +0100

    cliente

--- ejercicio1/main.py
+++ ejercicio1/main.py
@@ -2,8 +2,9 @@ from abstract import *
 from read import *
 from clientela import *
 
-
 def main():
+    print("Factoria 1")
+    cliente(Factoria1())
     print("Factoria 2")
     cliente(Factoria2())
 
