1c3e7b4ac91c9a389882bc09af477baffde08f0f
jzazooro <91785177+jzazooro@users.noreply.github.com>
2023-11-09 08:32:48 +0100

    lo que he podido

---
+++ ejercicio1/main.py
@@ -0,0 +1,13 @@
+from abstract import *
+from read import *
+from clientela import *
+
+
+def main():
+    print("Factoria 1")
+    cliente(Factoria1())
+    print("Factoria 2")
+    cliente(Factoria2())
+
+if __name__ == "__main__":
+    main()
